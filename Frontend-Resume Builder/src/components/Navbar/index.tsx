9fbfe3eb5acf429736666936d4207f3a05949d14
Alan0602 <alankanil1234@gmail.com>
2024-02-19 20:48:15 +0530

    feat(Navbar):Structure created

--- Frontend-Resume Builder/src/components/Navbar/index.tsx
+++ Frontend-Resume Builder/src/components/Navbar/index.tsx
@@ -1,7 +1,19 @@
-
+import styles from './index.module.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className={styles.navbarWrapper}>
+      <div className={styles.navbarHeading}>ResumeWiz</div>
+      <div className={styles.navbarLinks}>
+        <ul>
+          <li>Home</li>
+          <li>Template</li>
+          <li>About</li>
+        </ul>
+      </div>
+      <div className={styles.navbarButton}>
+        <button>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
--- Frontend-Resume Builder/src/modules/Home/index.tsx
+++ Frontend-Resume Builder/src/modules/Home/index.tsx
@@ -5,7 +5,6 @@ const Home = () => {
   return (
     <div>
         <Navbar/>
-      Homeindex
       </div>
   )
 }
