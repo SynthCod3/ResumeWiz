252220d344d19f497b96961d2b05bf455bc52169
Jenin82 <jenin8282@gmail.com>
2024-02-28 06:41:44 +0530

    feat: private route and supabase initial setup added

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,54 @@ import { Toaster } from "react-hot-toast";
 import Home from "./modules/Home";
 import SignIn from "./modules/SignIn";
 import SignUp from "./modules/SignUp";
+import NotFound from "./modules/notFound";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-          path: "/",
-          element: <Home />
-        },  
-        {
-            path: "/signIn",
-            element: <SignIn />
-          },  
-          {
-              path: "/signUp",
-              element: <SignUp />
-            }    
-    ]);
-    return (
-        <div className="App">
-            <RouterProvider router={router} />
-            <Toaster
-                position="bottom-center"
-                reverseOrder={false}
-                toastOptions={{
-                    success: {
-                        style: {
-                            background: "var(--border)",
-                            color: "var(--foreground)",
-                        },
-                    },
+	const router = createBrowserRouter([
+		{
+			path: "*",
+			element: <NotFound />,
+		},
+		{
+			path: "/404",
+			element: <NotFound />,
+		},
+		{
+			path: "/",
+			element: <Home />,
+		},
+		{
+			path: "/signIn",
+			element: <SignIn />,
+		},
+		{
+			path: "/signUp",
+			element: <SignUp />,
+		},
+	]);
+	return (
+		<div className="App">
+			<RouterProvider router={router} />
+			<Toaster
+				position="bottom-center"
+				reverseOrder={false}
+				toastOptions={{
+					success: {
+						style: {
+							background: "var(--border)",
+							color: "var(--foreground)",
+						},
+					},
 					error: {
 						style: {
 							background: "var(--border)",
 							color: "var(--foreground)",
 						},
-					}
-                }}
-            />
-        </div>
-    );
+					},
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
