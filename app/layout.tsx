74b99afe97bdd1cf171a4da34dcee7b0b9664a1f
Jenin82 <jenin8282@gmail.com>
2024-04-06 16:59:11 +0530

    feat: nav fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head />
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased pt-[4rem]',
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable,
         )}
       >
