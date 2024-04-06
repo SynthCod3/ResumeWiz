5ed76ed1648fad1e8671ff7ab16956c5dc054449
Alan0602 <alankanil1234@gmail.com>
2024-04-06 16:39:10 +0530

    Auth page content changed

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -7,6 +7,7 @@ const Authpage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
         <Navbar/>
+        <div className="mt-8"></div>
         <Auth/>
     </div>
   )
