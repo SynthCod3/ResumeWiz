53eb5207ec2a8420178e92710e3f2ba108338d38
Alan0602 <alankanil1234@gmail.com>
2024-04-07 08:56:17 +0530

    nothing

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { Auth } from "@/components/auth"
-import { Navbar } from "@/components/navbar"
-
-
-
-const Authpage = () => {
-  return (
-    <div className="flex h-screen items-center justify-center">
-        <Navbar/>
-        <div className="mt-8"></div>
-        <Auth/>
-    </div>
-  )
-}
-
-=======
 import { Auth } from "@/app/auth/components/auth"
 import { Navbar } from "@/components/navbar"
 
@@ -29,5 +12,4 @@ const Authpage = () => {
   )
 }
 
->>>>>>> a7fec8488aa0d0b000adfe51324f28b8252937f2
 export default Authpage
