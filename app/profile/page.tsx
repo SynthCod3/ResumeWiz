eec84517c194db01fd8be6d66140f7c7d19befbd
Alan0602 <alankanil1234@gmail.com>
2024-04-07 12:45:01 +0530

    created profile page for in route

---
+++ app/profile/page.tsx
@@ -0,0 +1,14 @@
+import { Profile } from "@/components/profile"
+import { Templates } from "../templates/components/templates"
+
+
+const page = () => {
+  return (
+    <div>
+        <Profile/>
+        <Templates/>
+    </div>
+  )
+}
+
+export default page
