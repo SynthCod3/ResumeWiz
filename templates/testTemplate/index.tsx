c0a1a774af23dafd052b23231a2cbe479291cc95
Alan0602 <alankanil1234@gmail.com>
2024-04-14 22:25:02 +0530

    set all current fields to test template

--- templates/testTemplate/index.tsx
+++ templates/testTemplate/index.tsx
@@ -1,53 +1,60 @@
 import * as React from 'react';
 import styles from './index.module.css';
+import { useEditorStore } from '@/utils/stores';
 
 const TestTemplate = () => {
+
+  const name = useEditorStore((state) => state.name);
+  const description = useEditorStore((state) => state.description);
+  const contact = useEditorStore((state) => state.contact);
+  const skills = useEditorStore((state) => state.skills);
+  const experience = useEditorStore((state) => state.experience);
+  const education = useEditorStore((state) => state.education);
+
   return (
     <div className={styles.Wrapper} style={{ aspectRatio: '1 / 1.414' }}>
       <div className="" id="resume">
         <div className="">
-          <h1>Jane Doe</h1>
-          <h2>Software Engineer</h2>
+          <h1>{name}</h1>
+          <h2></h2>
         </div>
         <div className="">
           <h2 className="">Contact Information</h2>
           <div className="">
-            <p>Email: janedoe@example.com</p>
-            <p>Phone: +1 (123) 456-7890</p>
-            <p>Address: 123 Street, City, Country</p>
+            <p>Email: {contact.email}</p>
+            <p>Phone: {contact.phone}</p>
+            <p>Address: {contact.address}</p>
           </div>
         </div>
         <div>
           <h2 className="">Summary</h2>
           <p className="">
-            Creative and detail-oriented software engineer with a passion for
-            developing innovative web applications. Experienced in full-stack
-            development and dedicated to writing clean and efficient code.
+            {description}
           </p>
         </div>
         <div>
           <h2 className="">Education</h2>
+            {education.map((edu) => (
           <p className="">
-            B.S. in Computer Science - University of Excellence (2010-2014)
-          </p>
+              <div>{edu.course} |({edu.endingDate}) |{edu.institution}| ({edu.university})| {edu.score} </div>
+          </p>)) }
+         
         </div>
         <div>
           <h2 className="">Work Experience</h2>
+            {experience.map((exp) => (
           <p className="">
-            Software Engineer - Tech Innovators Inc. (2014-2016)
-            <br />- Led the development of a cutting-edge e-commerce platform
-            <br />- Collaborated with cross-functional teams to deliver
-            high-quality software solutions
+              <div>{exp.company} | ({exp.endingDate}) | {exp.title} | {exp.description} </div>
           </p>
+            ))}
         </div>
         <div>
           <h2 className="">Skills</h2>
+
           <ul className="">
-            <li>JavaScript</li>
-            <li>React.js</li>
-            <li>Node.js</li>
-            <li>HTML/CSS</li>
-            <li>Agile Methodology</li>
+            {skills.map((skill) => (
+              <li>{skill}</li>
+            ))}
           </ul>
         </div>
       </div>
