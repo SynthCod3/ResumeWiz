bce64ae8b58feb8f9f60402507a958017075b4ef
Jenin <jenin8282@gmail.com>
2024-04-15 12:45:43 +0530

    chore: minor changes

--- templates/testTemplate/index.tsx
+++ templates/testTemplate/index.tsx
@@ -1,29 +1,32 @@
 import * as React from 'react';
 import styles from './index.module.css';
+import { useEditorStore } from '@/utils/stores';
 
 const TestTemplate = () => {
+  const name = useEditorStore((state) => state.name);
+  const description = useEditorStore((state) => state.description);
+  const contact = useEditorStore((state) => state.contact);
+  const skills = useEditorStore((state) => state.skills);
+  const experience = useEditorStore((state) => state.experience);
+
   return (
     <div className={styles.Wrapper} style={{ aspectRatio: '1 / 1.414' }}>
-      <div className="" id="resume">
-        <div className="">
-          <h1>Jane Doe</h1>
+      <div id="resume" className={styles.ResumeContainer}>
+        <div>
+          <h1 className={styles.heading}>{name}</h1>
           <h2>Software Engineer</h2>
         </div>
-        <div className="">
-          <h2 className="">Contact Information</h2>
+        <div>
+          <h2>Contact Information</h2>
           <div className="">
-            <p>Email: janedoe@example.com</p>
-            <p>Phone: +1 (123) 456-7890</p>
-            <p>Address: 123 Street, City, Country</p>
+            <p>Email: {contact.email}</p>
+            <p>Phone: {contact.phone}</p>
+            <p>Address: {contact.address}</p>
           </div>
         </div>
         <div>
           <h2 className="">Summary</h2>
-          <p className="">
-            Creative and detail-oriented software engineer with a passion for
-            developing innovative web applications. Experienced in full-stack
-            development and dedicated to writing clean and efficient code.
-          </p>
+          <p className="">{description}</p>
         </div>
         <div>
           <h2 className="">Education</h2>
@@ -33,12 +36,21 @@ const TestTemplate = () => {
         </div>
         <div>
           <h2 className="">Work Experience</h2>
-          <p className="">
-            Software Engineer - Tech Innovators Inc. (2014-2016)
-            <br />- Led the development of a cutting-edge e-commerce platform
-            <br />- Collaborated with cross-functional teams to deliver
-            high-quality software solutions
-          </p>
+          {experience.length > 0 ? (
+            experience.map((exp) => (
+              <p className="">
+                <b>{exp.title}</b> - {exp.company} ({exp.startingDate}-
+                {exp.endingDate})<p>{exp.description}</p>
+              </p>
+            ))
+          ) : (
+            <p className="">
+              Software Engineer - Tech Innovators Inc. (2014-2016)
+              <br />- Led the development of a cutting-edge e-commerce platform
+              <br />- Collaborated with cross-functional teams to deliver
+              high-quality software solutions
+            </p>
+          )}
         </div>
         <div>
           <h2 className="">Skills</h2>
